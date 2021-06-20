0da89d805852b11de8269f58616221f035457c06
Alvin Acosta <alvin.acosta@movon.com.ph>
2021-06-20 08:05:12 +0800

    item mapping and scrollview component

--- App.js
+++ App.js
@@ -1,30 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("Pauline");
-  const [age, setAge] = useState("34")
+  const [people, setPeople] = useState([
+    { name: "Alvin", key: 1 },
+    { name: "Pauline", key: 2 },
+    { name: "Pablo", key: 3 },
+    { name: "Kieran", key: 4 },
+    { name: "Thea", key: 5 },
+    { name: "Juts", key: 6 },
+    { name: "Randy", key: 7 },
+  ]);
+
+  console.log(people);
 
   return (
     <View style={styles.container}>
-      <Text>Enter name:</Text>
-      <TextInput
-        multiline
-        style={styles.input}
-        placeholder="Enter your name.." 
-        onChangeText={(value) => setName(value)}
-        />
-      <Text>Enter age:</Text>
-      <TextInput 
-        keyboardType="numeric"
-        style={styles.input}
-        placeholder="Enter your age.." 
-        onChangeText={(value) => setAge(value)}
-        />
+      <Text>JAGI FAMILY</Text>
 
-      <Text>My name is {name}, I am {age} years old.</Text>
-    
+      <ScrollView>
+        {people.map((item) => (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -32,15 +33,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
-  input : {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200
-  }
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "rebeccapurple",
+    fontSize: 24,
+  },
 });
