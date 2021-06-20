16850fd07ae3f563ba080a83a5330b11f941d6f7
Alvin Acosta <alvin.acosta@movon.com.ph>
2021-06-20 08:25:10 +0800

    flatlist, keys for flatlist and numcolumns

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
 
 export default function App() {
   const [people, setPeople] = useState([
-    { name: "Alvin", key: 1 },
-    { name: "Pauline", key: 2 },
-    { name: "Pablo", key: 3 },
-    { name: "Kieran", key: 4 },
-    { name: "Thea", key: 5 },
-    { name: "Juts", key: 6 },
-    { name: "Randy", key: 7 },
+    { name: "Alvin", id: 1 },
+    { name: "Pauline", id: 2 },
+    { name: "Pablo", id: 3 },
+    { name: "Kieran", id: 4 },
+    { name: "Thea", id: 5 },
+    { name: "Juts", id: 6 },
+    { name: "Randy", id: 7 },
   ]);
 
   console.log(people);
@@ -19,13 +19,28 @@ export default function App() {
     <View style={styles.container}>
       <Text>JAGI FAMILY</Text>
 
-      <ScrollView>
+      <FlatList
+        numColumns={2}
+        // keyExtractor={(item, index) => index.toString()}
+        // to remove warning about key of each list item
+        keyExtractor={(item) => item.id.toString()}
+        // this is used if there is no key property. indicate the property you want to
+        // be used as key. in this case the id like in most databases you will use
+        data={people}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      />
+
+      {/* flatlists lazy loads so its better for more items on the list - performance */}
+
+      {/* <ScrollView>
         {people.map((item) => (
           <View key={item.key}>
             <Text style={styles.item}>{item.name}</Text>
           </View>
         ))}
-      </ScrollView>
+      </ScrollView> */}
+
+      {/* ScrollView is fine for smaller lists and if you just need scrollability */}
     </View>
   );
 }
@@ -44,5 +59,7 @@ const styles = StyleSheet.create({
     padding: 30,
     backgroundColor: "rebeccapurple",
     fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24,
   },
 });
